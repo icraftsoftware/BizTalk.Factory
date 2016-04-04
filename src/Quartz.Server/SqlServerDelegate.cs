d637b9477f14bfbc091bce7510e9ed242c839dae
ydie_cp <7443208+ydie22@users.noreply.github.com>
2016-04-04 09:17:43 +0000

    Adding Quartz SQLServerDelegate that stores fully qualified class name for job classes

---
+++ src/Quartz.Server/SqlServerDelegate.cs
@@ -0,0 +1,33 @@
+﻿#region Copyright & License
+// Copyright © 2012 - 2016 François Chabot, Yves Dierick
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+
+namespace Quartz.Server
+{
+	/// <summary>
+	/// This class allows storing the fully qualified name of Quartz job classes in 
+	/// the configuration database. This is necessary to allow Quartz to load
+	/// those classes when they are in an assembly deployed in the GAC.
+	/// </summary>
+	public class SqlServerDelegate : Impl.AdoJobStore.SqlServerDelegate
+	{
+		protected override string GetStorableJobTypeName(Type jobType)
+		{
+			return jobType.AssemblyQualifiedName;
+		}
+	}
+}
