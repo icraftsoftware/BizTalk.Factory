fa0da41a663c72443cb388952e6366a75c34d24d
François Chabot <icraftsoftware@users.noreply.github.com>
2016-12-19 12:58:12 +0000

    BizTalk.Factory.1.0.16354

--- src/Version.cs
+++ src/Version.cs
@@ -18,6 +18,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("1.0.16344.43927")]
-[assembly: AssemblyInformationalVersion("1.0.16344.43927")]
+[assembly: AssemblyFileVersion("1.0.16354.35819")]
+[assembly: AssemblyInformationalVersion("1.0.16354.35819")]
 [assembly: AssemblyVersion("1.0.0.0")]
