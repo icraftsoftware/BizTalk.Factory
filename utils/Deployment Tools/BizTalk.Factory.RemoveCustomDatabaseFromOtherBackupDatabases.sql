dc298ca532d45f920549919003a2e88d28a3952a
François Chabot <icraftsoftware@users.noreply.github.com>
2017-01-31 08:30:36 +0000

    UTF8 file encoding issue

--- utils/Deployment Tools/BizTalk.Factory.RemoveCustomDatabaseFromOtherBackupDatabases.sql
+++ utils/Deployment Tools/BizTalk.Factory.RemoveCustomDatabaseFromOtherBackupDatabases.sql
@@ -1,5 +1,5 @@
-/*
-Copyright © 2012 - 2014 François Chabot, Yves Dierick
+ï»¿/*
+Copyright Â© 2012 - 2014 FranÃ§ois Chabot, Yves Dierick
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
