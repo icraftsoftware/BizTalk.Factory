f9d0f4b1b2ac5c91983278313d33885d05cd915e
François Chabot <icraftsoftware@users.noreply.github.com>
2017-09-18 14:52:16 +0000

    BizTalk.Factory.1.0.17261.43374 Build Tools.

--- src/Version.cs
+++ src/Version.cs
@@ -18,6 +18,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("1.0.17261.43175")]
-[assembly: AssemblyInformationalVersion("1.0.17261.43175")]
+[assembly: AssemblyFileVersion("1.0.17261.43374")]
+[assembly: AssemblyInformationalVersion("1.0.17261.43374")]
 [assembly: AssemblyVersion("1.0.0.0")]
