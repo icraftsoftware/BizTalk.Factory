c8133434aa65e4aed8eb03f67ee7765f55432066
icraftsoftware <fchabot@skynet.be>
2017-11-11 13:08:51 +0100

    Ensure InvokeBuild module is available

--- build.ps1
+++ build.ps1
@@ -1,1 +1,7 @@
-Invoke-Build -File .\.build\BizTalk.Factory.build.ps1 -Task .
+# ensure InvokeBuild module is available
+if (-not(Get-Module -ListAvailable | Where-Object Name -eq InvokeBuild)) {
+   Install-Module -Name InvokeBuild -Force -Scope CurrentUser
+}
+if ($?) {
+   Invoke-Build -File .\.build\BizTalk.Factory.build.ps1 -Task .
+}
