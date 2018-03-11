3e4dc20d3fe0fc449acaf446511f2bf036890077
icraftsoftware <fchabot@skynet.be>
2018-03-11 17:16:04 +0100

    MultipartFormDataContentEncoder & MultipartFormDataContentStream

---
+++ src/BizTalk.Common.Tests/Streaming/MultipartFormDataContentStreamFixture.cs
@@ -0,0 +1,82 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2018 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System.IO;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+
+namespace Be.Stateless.BizTalk.Streaming
+{
+	[TestFixture]
+	public class MultipartFormDataContentStreamFixture
+	{
+		[Test]
+		public void ClosesWrappedStream()
+		{
+			var streamMock = new Mock<MemoryStream>(MockBehavior.Default, Encoding.UTF8.GetBytes(CONTENT)) { CallBase = true };
+
+			using (var composite = new MultipartFormDataContentStream(streamMock.Object))
+			{
+				var buffer = new byte[10];
+				composite.Read(buffer, 0, buffer.Length);
+			}
+
+			streamMock.Verify(s => s.Close());
+		}
+
+		[Test]
+		public void ExhaustsWrappedStream()
+		{
+			using (var stream = new MultipartFormDataContentStream(new MemoryStream(Encoding.UTF8.GetBytes(CONTENT))))
+			{
+				var buffer = new byte[256];
+				var offset = 0;
+				int bytesRead;
+				while ((bytesRead = stream.Read(buffer, offset, 8)) > 0)
+				{
+					offset += bytesRead;
+				}
+
+				var actual = Encoding.UTF8.GetString(buffer, 0, offset);
+				Assert.That(actual, Does.Match(EXPECTED_CONTENT_PATTERN));
+
+				var trail = Encoding.UTF8.GetString(buffer, offset, buffer.Length - offset);
+				Assert.That(trail.ToCharArray(), Has.All.EqualTo('\0'));
+			}
+		}
+
+		[Test]
+		public void MultipartEncodesWrappedStream()
+		{
+			using (var stream = new MultipartFormDataContentStream(new MemoryStream(Encoding.UTF8.GetBytes(CONTENT))))
+			using (var reader = new StreamReader(stream))
+			{
+				var actual = reader.ReadToEnd();
+
+				Assert.That(actual, Does.Match(EXPECTED_CONTENT_PATTERN));
+			}
+		}
+
+		private const string CONTENT = "<part-one xmlns=\"part-one\"><child-one>one</child-one></part-one>";
+
+		private const string EXPECTED_CONTENT_PATTERN = "^(--[a-f\\d]{8}-[a-f\\d]{4}-[a-f\\d]{4}-[a-f\\d]{4}-[a-f\\d]{12})\r\nContent-Disposition: form-data\r\n\r\n"
+			+ CONTENT
+			+ "\r\n\\1--\r\n$";
+	}
+}
---
+++ src/BizTalk.Common/Streaming/MultipartFormDataContentStream.cs
@@ -0,0 +1,99 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2018 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace Be.Stateless.BizTalk.Streaming
+{
+	public class MultipartFormDataContentStream : Stream
+	{
+		public MultipartFormDataContentStream(Stream stream)
+		{
+			_multipartContent = new MultipartFormDataContent { new StreamContent(stream) };
+			_stream = _multipartContent.ReadAsStreamAsync().Result;
+		}
+
+		#region Base Class Member Overrides
+
+		public override bool CanRead
+		{
+			get { return true; }
+		}
+
+		public override bool CanSeek
+		{
+			get { return _stream.CanSeek; }
+		}
+
+		public override bool CanWrite
+		{
+			get { return false; }
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && _multipartContent != null)
+			{
+				_stream.Dispose();
+				_stream = null;
+				_multipartContent.Dispose();
+				_multipartContent = null;
+			}
+			base.Dispose(disposing);
+		}
+
+		public override void Flush()
+		{
+			_stream.Flush();
+		}
+
+		public override long Length
+		{
+			get { return _stream.Length; }
+		}
+
+		public override long Position { get; set; }
+
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			return _stream.Read(buffer, offset, count);
+		}
+
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			return _stream.Seek(offset, origin);
+		}
+
+		public override void SetLength(long value)
+		{
+			_stream.SetLength(value);
+		}
+
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			throw new NotSupportedException();
+		}
+
+		#endregion
+
+		private MultipartFormDataContent _multipartContent;
+		private Stream _stream;
+	}
+}
