1104c6cfd530a1f7c3e579a19b9546acec493ad1
icraftsoftware <fchabot@skynet.be>
2018-11-07 18:31:02 +0100

    SBMessagingContextPropagator

---
+++ src/BizTalk.Pipeline.Components.Tests/MicroComponent/SBMessagingContextPropagatorFixture.cs
@@ -0,0 +1,155 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2018 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using Be.Stateless.BizTalk.ContextProperties;
+using Be.Stateless.BizTalk.Message.Extensions;
+using Be.Stateless.BizTalk.Unit.MicroComponent;
+using Moq;
+using NUnit.Framework;
+
+namespace Be.Stateless.BizTalk.MicroComponent
+{
+	[TestFixture]
+	public class SBMessagingContextPropagatorFixture : MicroPipelineComponentFixture
+	{
+		[Test]
+		public void BizTalkPropertiesAreOnlyPropagatedOutward()
+		{
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.InboundTransportLocation)).Returns("inbound-transport-location");
+			MessageMock.Setup(m => m.GetProperty(BizTalkFactoryProperties.CorrelationToken)).Returns(Guid.NewGuid().ToString);
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.MessageType)).Returns("urn:ns#root");
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.SetProperty(SBMessagingProperties.CorrelationId, It.IsAny<string>()), Times.Never);
+			MessageMock.Verify(m => m.SetProperty(SBMessagingProperties.ContentType, It.IsAny<string>()), Times.Never);
+		}
+
+		[Test]
+		public void ContentTypeIsNotPropagatedInwardWhenEmpty()
+		{
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.InboundTransportLocation)).Returns("inbound-transport-location");
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.SetProperty(BtsProperties.MessageType, It.IsAny<string>()), Times.Never);
+		}
+
+		[Test]
+		public void ContentTypeIsPropagatedInward()
+		{
+			var messageType = "urn:ns#root";
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.InboundTransportLocation)).Returns("inbound-transport-location");
+			MessageMock.Setup(m => m.GetProperty(SBMessagingProperties.ContentType)).Returns(messageType);
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.SetProperty(BtsProperties.MessageType, messageType), Times.Once);
+		}
+
+		[Test]
+		public void CorrelationIdIsNotPropagatedInwardWhenEmpty()
+		{
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.InboundTransportLocation)).Returns("inbound-transport-location");
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.Promote(BizTalkFactoryProperties.CorrelationToken, It.IsAny<string>()), Times.Never);
+		}
+
+		[Test]
+		public void CorrelationIdIsPropagatedInward()
+		{
+			var token = Guid.NewGuid().ToString();
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.InboundTransportLocation)).Returns("inbound-transport-location");
+			MessageMock.Setup(m => m.GetProperty(SBMessagingProperties.CorrelationId)).Returns(token);
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.Promote(BizTalkFactoryProperties.CorrelationToken, token), Times.Once);
+		}
+
+		[Test]
+		public void CorrelationTokenIsNotPropagatedOutwardWhenEmpty()
+		{
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.OutboundTransportLocation)).Returns("outbound-transport-location");
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.SetProperty(SBMessagingProperties.CorrelationId, It.IsAny<string>()), Times.Never);
+		}
+
+		[Test]
+		public void CorrelationTokenIsPropagatedOutward()
+		{
+			var token = Guid.NewGuid().ToString();
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.OutboundTransportLocation)).Returns("outbound-transport-location");
+			MessageMock.Setup(m => m.GetProperty(BizTalkFactoryProperties.CorrelationToken)).Returns(token);
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.SetProperty(SBMessagingProperties.CorrelationId, token), Times.Once);
+		}
+
+		[Test]
+		public void MessageTypeIsNotPropagatedOutwardWhenEmpty()
+		{
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.OutboundTransportLocation)).Returns("outbound-transport-location");
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.SetProperty(BtsProperties.MessageType, It.IsAny<string>()), Times.Never);
+		}
+
+		[Test]
+		public void MessageTypeIsPropagatedOutward()
+		{
+			var messageType = "urn:ns#root";
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.OutboundTransportLocation)).Returns("outbound-transport-location");
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.MessageType)).Returns(messageType);
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.SetProperty(SBMessagingProperties.ContentType, messageType), Times.Once);
+		}
+
+		[Test]
+		public void SBMessagingPropertiesAreOnlyPropagatedInward()
+		{
+			MessageMock.Setup(m => m.GetProperty(BtsProperties.OutboundTransportLocation)).Returns("outbound-transport-location");
+			MessageMock.Setup(m => m.GetProperty(SBMessagingProperties.CorrelationId)).Returns(Guid.NewGuid().ToString);
+			MessageMock.Setup(m => m.GetProperty(SBMessagingProperties.ContentType)).Returns("urn:ns#root");
+
+			var sut = new SBMessagingContextPropagator();
+			sut.Execute(PipelineContextMock.Object, MessageMock.Object);
+
+			MessageMock.Verify(m => m.SetProperty(BizTalkFactoryProperties.CorrelationToken, It.IsAny<string>()), Times.Never);
+			MessageMock.Verify(m => m.SetProperty(BtsProperties.MessageType, It.IsAny<string>()), Times.Never);
+		}
+	}
+}
---
+++ src/BizTalk.Pipeline.Components/MicroComponent/SBMessagingContextPropagator.cs
@@ -0,0 +1,54 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2018 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using Be.Stateless.BizTalk.Component;
+using Be.Stateless.BizTalk.ContextProperties;
+using Be.Stateless.BizTalk.ContextProperties.Extensions;
+using Be.Stateless.BizTalk.Message.Extensions;
+using Be.Stateless.Extensions;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+
+namespace Be.Stateless.BizTalk.MicroComponent
+{
+	public class SBMessagingContextPropagator : IMicroPipelineComponent
+	{
+		#region IMicroPipelineComponent Members
+
+		public IBaseMessage Execute(IPipelineContext pipelineContext, IBaseMessage message)
+		{
+			if (message.Direction().IsInbound())
+			{
+				var correlationId = message.GetProperty(SBMessagingProperties.CorrelationId);
+				if (!correlationId.IsNullOrEmpty()) message.PromoteCorrelationToken(correlationId);
+				var contentType = message.GetProperty(SBMessagingProperties.ContentType);
+				if (!contentType.IsNullOrEmpty()) message.SetProperty(BtsProperties.MessageType, contentType);
+			}
+			else
+			{
+				var correlationToken = message.GetProperty(BizTalkFactoryProperties.CorrelationToken);
+				if (!correlationToken.IsNullOrEmpty()) message.SetProperty(SBMessagingProperties.CorrelationId, correlationToken);
+				var messageType = message.GetProperty(BtsProperties.MessageType);
+				if (!messageType.IsNullOrEmpty()) message.SetProperty(SBMessagingProperties.ContentType, messageType);
+			}
+			return message;
+		}
+
+		#endregion
+	}
+}
