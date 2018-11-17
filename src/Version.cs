7f856baf2e3e237f46f3960b91e51091271f7c2f
icraftsoftware <fchabot@skynet.be>
2018-11-17 15:26:20 +0100

    Update BizTalk.Factory Build Tools 1.0.18321.39608

--- src/Version.cs
+++ src/Version.cs
@@ -18,6 +18,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("1.0.18311.47930")]
-[assembly: AssemblyInformationalVersion("1.0.18311.47930")]
+[assembly: AssemblyFileVersion("1.0.18321.39608")]
+[assembly: AssemblyInformationalVersion("1.0.18321.39608")]
 [assembly: AssemblyVersion("1.0.0.0")]
